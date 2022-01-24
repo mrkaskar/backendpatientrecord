3d83b83cb66e8b733c11ca33b922943f0620321a
Kyaw Zin Thant <66065310+kzinthant-d3v@users.noreply.github.com>
2022-01-24 15:13:52 +0630

    Update envconfig.js

--- envconfig.js
+++ envconfig.js
@@ -1,6 +1,6 @@
 const isProduction = !!process.env.NODE_ENV;
 const env = {
-    folderId: isProduction ? process.env.FOLDER_ID : '1B_Qfjlz8KayC5R5bmL_nd3NehFwKaUnb',
-    database: isProduction ? process.env.DATABASE : "mongodb+srv://dbuser:dd123@cluster0.hvpaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    folderId: isProduction ? process.env.FOLDER_ID : "",
+    database: isProduction ? process.env.DATABASE : ""
 }
 module.exports = env;
